324b473bcfb567f91492b5b78235ca678e86e30e
yoshixmk <pirori67@gmail.com>
2017-04-07 23:02:33 +0900

    x,y,zから最大値をとる問題を解いた。file rename

--- 1-1/x_y_max.sql
+++ 1-1/x_y_max.sql
@@ -1,3 +1,4 @@
+-- xとyの最大値をとる
 SELECT 
     id,
     CASE
@@ -6,3 +7,15 @@ SELECT
     END AS greatest
 FROM
     exercises.greatests;
+    
+-- xとyとzから最大値をとる
+SELECT 
+    id,
+    CASE
+        WHEN x > y AND x > z THEN x
+        WHEN y > z THEN y
+        ELSE z
+    END AS greatest
+FROM
+    exercises.greatests;
+
